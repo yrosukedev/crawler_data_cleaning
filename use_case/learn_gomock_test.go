81e3e8be1b96e9229d6e10567525fac02410bed5
shrek wang <shrekwang1@163.com>
2023-01-29 20:52:59 +0800

    Learn mock library: function called.

---
+++ use_case/learn_gomock_test.go
@@ -0,0 +1,26 @@
+package use_case
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/yrosukedev/crawler_data_cleaning/mock_use_case"
+	"testing"
+)
+
+type Foo interface {
+	Bar(x int) int
+}
+
+func TestLearnMock_functionCalled(t *testing.T) {
+	// Given
+	ctrl := gomock.NewController(t)
+	f := mock_use_case.NewMockFoo(ctrl)
+
+	// Then
+	f.
+		EXPECT().
+		Bar(gomock.Eq(99)).
+		Times(1)
+
+	// When
+	f.Bar(99)
+}
